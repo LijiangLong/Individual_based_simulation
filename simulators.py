f96771bfff2e5d38718c5d4dc67f61b046a72439
LijiangLong <lijiang@ipsec-143-215-28-160.vpn.gatech.edu>
2018-11-11 14:16:05 -0500

    n

--- simulators.py
+++ simulators.py
@@ -28,7 +28,7 @@ def Neutral_Evolution_with_incompatible_gene_outcrossing1(p):
     
 def Neutral_Evolution_with_incompatible_gene_outcrossing001(p):
     np.random.seed(p)
-    population_simulator = population(growth_speed_array = [1,1,1],incompatible = 1,outcrossing_rate = 0.01,max_generation=5)
+    population_simulator = population(growth_speed_array = [1,1,1],incompatible = 1,outcrossing_rate = 0.01,max_generation=2000)
     return population_simulator.evolve_until_fix()
 
 def Neutral_Evolution_with_incompatible_gene_outcrossing001_large_population(p):
