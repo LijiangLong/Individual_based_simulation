dc6d19620eb13bee24e0367825f81c242260dfdc
LijiangLong <lijiang@ipsec-143-215-28-151.vpn.gatech.edu>
2018-11-10 19:27:03 -0500

    new

--- simulators.py
+++ simulators.py
@@ -20,9 +20,7 @@ def heterozygous_advantage_simulation(p):
     population_simulator = population(growth_speed_array = [1,1.02,1])
     return population_simulator.evolve_until_fix()
     
-    
-    
-    
+
 def Neutral_Evolution_with_incompatible_gene_outcrossing1(p):
     np.random.seed(p)
     population_simulator = population(growth_speed_array = [1,1,1],incompatible = 1)
@@ -31,4 +29,14 @@ def Neutral_Evolution_with_incompatible_gene_outcrossing1(p):
 def Neutral_Evolution_with_incompatible_gene_outcrossing001(p):
     np.random.seed(p)
     population_simulator = population(growth_speed_array = [1,1,1],incompatible = 1,outcrossing_rate = 0.01)
+    return population_simulator.evolve_until_fix()
+
+def Neutral_Evolution_with_incompatible_gene_outcrossing001_large_population(p):
+    np.random.seed(p)
+    population_simulator = population(population_size = 10000,growth_speed_array = [1,1,1],incompatible = 1,outcrossing_rate = 0.01)
+    return population_simulator.evolve_until_fix()
+
+def Neutral_Evolution_with_incompatible_gene_outcrossing001_large_population_fitness_balancing(p):
+    np.random.seed(p)
+    population_simulator = population(population_size = 10000,growth_speed_array = [1,1.04,1.08],incompatible = 1,outcrossing_rate = 0.01)
     return population_simulator.evolve_until_fix()
